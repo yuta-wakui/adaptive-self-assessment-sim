7b111023df420bd056d0592c45026ceae85f52e9
yuta46 <yuta461031@gmail.com>
2026-02-15 02:48:17 +0900

    [add]run_non_adaptive_ws1_simulation, [add]run_non_adaptive_ws2_simulation,

--- src/adaptive_self_assessment/simulation/__init__.py
+++ src/adaptive_self_assessment/simulation/__init__.py
@@ -10,12 +10,12 @@ Simulation utilities for adaptive self-assessment.
 from .common import load_app_config
 from .adaptive_ws1 import run_ws1_simulation
 from .adaptive_ws2 import run_ws2_simulation
-# from .non_adaptive_ws1 import run_non_adaptive_ws1_simulation
-# from .non_adaptive_ws2 import run_non_adaptive_ws2_simulation
+from .non_adaptive_ws1 import run_non_adaptive_ws1_simulation
+from .non_adaptive_ws2 import run_non_adaptive_ws2_simulation
 
 __all__ = [
     "run_ws1_simulation",
     "run_ws2_simulation",
-    # "run_non_adaptive_ws1_simulation",
-    # "run_non_adaptive_ws2_simulation",
+    "run_non_adaptive_ws1_simulation",
+    "run_non_adaptive_ws2_simulation",
 ]
