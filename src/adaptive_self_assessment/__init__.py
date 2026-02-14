48539f04fc75555286014f87c3497e138450549d
yuta46 <yuta461031@gmail.com>
2026-02-15 02:48:45 +0900

    [add]run_non_adaptive_ws1_simulation, [add]run_non_adaptive_ws2_simulation,

--- src/adaptive_self_assessment/__init__.py
+++ src/adaptive_self_assessment/__init__.py
@@ -7,9 +7,13 @@ Machine learning-driven adaptive self-assessment library
 from adaptive_self_assessment.simulation.common import load_app_config
 from adaptive_self_assessment.simulation.adaptive_ws1 import run_ws1_simulation
 from adaptive_self_assessment.simulation.adaptive_ws2 import run_ws2_simulation
+from adaptive_self_assessment.simulation.non_adaptive_ws1 import run_non_adaptive_ws1_simulation
+from adaptive_self_assessment.simulation.non_adaptive_ws2 import run_non_adaptive_ws2
 
 __all__ = [
     "load_app_config",
     "run_ws1_simulation",
     "run_ws2_simulation",
+    "run_non_adaptive_ws1_simulation",
+    "run_non_adaptive_ws2_simulation",
 ]
