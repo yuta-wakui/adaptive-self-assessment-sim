8ecdbbaf401b91357d35b59a31ba6915ac0f74a6
yuta46 <yuta461031@gmail.com>
2026-02-19 21:59:58 +0900

    all commit

--- src/adaptive_self_assessment/__init__.py
+++ src/adaptive_self_assessment/__init__.py
@@ -8,7 +8,7 @@ from adaptive_self_assessment.simulation.common import load_app_config
 from adaptive_self_assessment.simulation.adaptive_ws1 import run_ws1_simulation
 from adaptive_self_assessment.simulation.adaptive_ws2 import run_ws2_simulation
 from adaptive_self_assessment.simulation.non_adaptive_ws1 import run_non_adaptive_ws1_simulation
-from adaptive_self_assessment.simulation.non_adaptive_ws2 import run_non_adaptive_ws2
+from adaptive_self_assessment.simulation.non_adaptive_ws2 import run_non_adaptive_ws2_simulation
 
 __all__ = [
     "load_app_config",
